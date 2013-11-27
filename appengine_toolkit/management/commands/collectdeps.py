000f359a95677c614eddaa91d8d25e0b7989fac8
masci <mpippi@gmail.com>
2013-11-28 00:02:27 +0100

    minor

--- appengine_toolkit/management/commands/collectdeps.py
+++ appengine_toolkit/management/commands/collectdeps.py
@@ -1,8 +1,7 @@
 import sys
+from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
-
-from optparse import make_option
 
 from ._utils import collect_dependency_paths, parse_requirements_file, RequirementNotFoundError, make_simlinks
 
