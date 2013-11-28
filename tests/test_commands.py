f9efa0d4b6f7452482cb3759c323f0194658d74e
masci <mpippi@gmail.com>
2013-11-28 17:52:42 +0100

    run tests with interactive mode off

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -10,12 +10,12 @@ class TestCommands(TestCase):
 
 class TestCollectDeps(TestCommands):
     def test_call_wrong(self):
-        self.assertRaises(CommandError, call_command, 'collectdeps')
+        self.assertRaises(CommandError, call_command, 'collectdeps', interactive=False)
 
     def test_call_package(self):
         with mock.patch('appengine_toolkit.management.commands.collectdeps.collect_dependency_paths') as mock_collect:
             mock_collect.return_value = []
-            call_command('collectdeps', 'foo')
+            call_command('collectdeps', 'foo', interactive=False)
             self.assertEqual(mock_collect.call_args[0], ('foo',))
 
     def test_call_req_file(self):
@@ -24,12 +24,10 @@ class TestCollectDeps(TestCommands):
                 mock.patch('appengine_toolkit.management.commands.collectdeps.collect_dependency_paths') as mock_collect:
             mock_parse.return_value = ['foo']
             mock_collect.return_value = []
-            call_command('collectdeps', requirements_file='foofile')
+            call_command('collectdeps', requirements_file='foofile', interactive=False)
 
     def test_call_req_file_fail(self):
         with mock.patch('__builtin__.open'), \
                 mock.patch('appengine_toolkit.management.commands.collectdeps.parse_requirements_file') as mock_parse:#, \
-                #mock.patch('appengine_toolkit.management.commands.collectdeps.collect_dependency_paths') as mock_collect:
             mock_parse.return_value = ['foo']
-            #mock_collect.return_value = ['foo']
-            self.assertRaises(CommandError, call_command, 'collectdeps', requirements_file='foofile')
+            self.assertRaises(CommandError, call_command, 'collectdeps', requirements_file='foofile', interactive=False)
