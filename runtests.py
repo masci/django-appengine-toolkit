49e2db39e8867a14aef6c3fce1ddde0f535b7fbd
masci <mpippi@gmail.com>
2013-12-30 22:21:31 +0100

    set pat to gae dependencies

--- runtests.py
+++ runtests.py
@@ -2,7 +2,27 @@ import sys
 import os
 from optparse import OptionParser
 
+import google
+
 here = os.path.dirname(os.path.abspath(__file__))
+gae_path = os.path.dirname(os.path.abspath(google.__file__))
+dir_path = os.path.join(gae_path, '..')
+
+GAE_LIBRARY_PATHS = [
+    dir_path,
+    os.path.join(dir_path, 'lib', 'cherrypy'),
+    os.path.join(dir_path, 'lib', 'fancy_urllib'),
+    os.path.join(dir_path, 'lib', 'yaml-3.10'),
+    os.path.join(dir_path, 'lib', 'antlr3'),
+    os.path.join(dir_path, 'lib', 'concurrent'),
+    os.path.join(dir_path, 'lib', 'ipaddr'),
+    os.path.join(dir_path, 'lib', 'jinja2-2.6'),
+    os.path.join(dir_path, 'lib', 'webob-1.2.3'),
+    os.path.join(dir_path, 'lib', 'webapp2-2.5.1'),
+    os.path.join(dir_path, 'lib', 'mox'),
+    os.path.join(dir_path, 'lib', 'protorpc-1.0'),
+    os.path.join(dir_path, 'lib', 'simplejson'),
+]
 
 try:
     from django.conf import settings
@@ -27,7 +47,9 @@ try:
         APPENGINE_TOOLKIT={
             'APP_YAML': os.path.join(here, 'tests', 'foo.yaml'),
             'BUCKET_NAME': 'test_bucket',
-        }
+        },
+        DEFAULT_FILE_STORAGE='appengine_toolkit.storage.GoogleCloudStorage',
+        NOSE_PLUGINS=['nosegae.NoseGAE'],
     )
 
     from django_nose import NoseTestSuiteRunner
@@ -49,6 +71,14 @@ def run_tests(*test_args):
 
 
 if __name__ == '__main__':
+    sys.path.extend(GAE_LIBRARY_PATHS)
+
+    from google.appengine.tools import dev_appserver
+    from google.appengine.tools.dev_appserver_main import ParseArguments
+
+    option_dict = ParseArguments(sys.argv)[1]
+    dev_appserver.SetupStubs('local', **option_dict)
+
     parser = OptionParser()
     (options, args) = parser.parse_args()
     run_tests(*args)
